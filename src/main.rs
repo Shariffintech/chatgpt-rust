1d0f0462c7a034bab0e422d7b6a9abf1c64376ee
Sharif.T <Shariftarver@gmail.com>
2023-03-22 15:40:39 -0400

    loop,read user input+ auth / client to make uri request

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,97 @@ use serde_derive::{Deserialize,Serialize};
 use spinners::{Spinners, Spinners};
 use std::io::{stdin, stdout, Read, Write};
 
+//struct to work with the api response
+#[derive(Serialize, Deserialize)]
+struct OAIResponse {
+    id: Option<String>,
+    object: String,
+    created:Option<u64>,
+    model: Option<String>,
+    choices: Vec<OAIChoices>,
+}
+
 
 //a struct for the choices
 #[derive(Serialize, Deserialize)]
 struct OAIChoices {
-    choices: Vec<String>,
+    text: String,
+    index: u8,
+    logprob: option<u8>,
+    finish_reason: String,
 }
 
 
 //struct for request to api
-//structs to work with the api response
+#[derive(Serialize, Deserialize)]
+struct OAIRequestdata {
+    prompt: String,
+    //number of words to generate, not actual tokens
+    max_tokens: u16,
+    temperature: f32,
+    frequency: f32,
+}
 
-// actix async main function
-// load env variables
-//create httpconnector, hyper
-//create httpclient, hyper
-//url to make the request
+// tokio async main function
+#[tokio::main]
+async fn main()-> Result<(), Box<dyn std::error::Error + send + Sync>> {
+    dotenv.ok();
+    //create httpconnector, hyper
+    let https = HttpsConnector::new(4)?;
+    let client = Client::builder().build(https);
+    
+    //get the api url
+    let api_url = "http://api.openai.com/v1/engines/text-davinci-002/completions";
+    let preamble = "What is a lifetime in rust?";
+    let oai_token = String = env::var("OPENAI_TOKEN").unwrap();
+    let auth_header_value = format!("Bearer {}", oai_token);
+    println!("{esc}c", esc = 27 as char);
+
+    loop{
+        //prompt user for input
+        println!("<>");
+        stdout().flush().unwrap();
+        let mut user_text = String::new();
+
+        //read user input
+        stdin()
+            .read_line(&mut user_text)
+            .expect( "Failed to read line")?;
+            println!("");
+
+             //spinners
+            let spin_spin = Spinners::new(Spinners::Chars("|/-\\"));
+            spin_spin.set_message("Loading...");
+            spin_spin.start();
+           
+
+        //send request to api
+        let request_data = OAIRequestdata{
+            prompt: format!("{}{}", preamble, user_text),
+            max_tokens: 600,
+            temperature: 1.0,
+            frequency: 1.0,
+        } 
+
+
+          //url to make the request
+          api_request.headers_mut().set(header::CONTENT_TYPE, "application/json");
+          let mut api_request = Request::new(Body::empty());
+    }
+
+
+
+  
+    
+    
+    
+    
+ 
+    
+}
 // prompt for user input to chatgpt
+// load env variables
+//create httpclient, hyper
 // token, in the header
 
 //loop  - inside the loop to read user input
